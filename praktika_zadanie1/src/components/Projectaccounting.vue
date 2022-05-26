e08ad2ef4aff37425551bf962d66b1ad8b203f66
egrom2012 <eg.rom2012@narod.ru>
2022-05-26 20:05:54 +0300

    Update Projectaccounting.vue

--- praktika_zadanie1/src/components/Projectaccounting.vue
+++ praktika_zadanie1/src/components/Projectaccounting.vue
@@ -1,103 +1,97 @@
 <template>
-  <div>
     <form @submit.prevent="onsubmit">
-      <input type="text" class="textcg" v-model="title" />
-      <button type="submit" class="butcg">Создать новую задачу</button>
+        <input type="text" class="textcg" v-model="title" />
+        <button type="submit" class="butcg">Создать новую задачу</button>
     </form>
     <div>
-      <pre>editedId:{{ editedId }}</pre>
-      <ul>
-        <li
-          v-for="(i, f) of spisok"
-          v-bind:class="{ done: i.completed }"
-          :key="i.id"
-        >
-          <span>
-            <input type="checkbox" v-on:change="i.completed = !i.completed" />
-            <strong> {{ f + 1 }} </strong>
-            <input v-if="editedId === i.id" type="text" :value="i.title" />
-            <span v-else>{{ i.title }} </span>
-          </span>
-          <button class="ra" @click="editedId = i.id">Изменить</button>
-          <button class="rm" v-on:click="$emit('remove-project', i.id)">
-            &times;
-          </button>
-        </li>
-      </ul>
+        <ul>
+            <li v-for="(i,f) in spisok" v-bind:class="{done:i.completed}">
+                <span>
+                    <input type="checkbox"
+                           v-on:change="i.completed = !i.completed" />
+                    <strong> {{f+1}} </strong>
+                    <input type="text" v-if="i.isEditing" @keyup.enter="$emit('edit-Project',i.title)" v-model=editValue>
+                    <span v-else> {{i.title}} </span>
+                </span>
+                <button @click="$emit ('change-Editing',i.title)">Изменить</button>
+                <button class="rm" v-on:click="$emit ('remove-project',i.id)">&times;</button>
+
+            </li>
+        </ul>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  props: ["spisok"],
-  f: Number,
-  data: () => ({
-    title: "",
-    editedId: null,
-  }),
+    export default
+        {
+            props: ['spisok'],
+            f: Number,
+            data() {
+                return
+                {
+                    title: ''
+                }
+            },
 
-  methods: {
-    onsubmit() {
-      if (this.title.trim()) {
-        const newproject = {
-          title: this.title,
-          id: Date.now(),
-          completed: false,
-        };
-        this.$emit("add-project", newproject);
-        this.title = "";
-      }
-    },
-  },
-};
+            methods:
+            {
+                onsubmit() {
+                    if (this.title.trim()) {
+                        const newproject = { title: this.title, id: Date.now(), completed: false, isEditing: false }
+                        this.$emit('add-project', newproject)
+                        this.title = ''
+                    }
+                }
+            }
+        }
 </script>
 
 <style scoped>
-ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
 
-li {
-  border: 1px solid #ccc;
-  display: flex;
-  justify-content: space-between;
-  padding: 0.5rem 2rem;
-  margin-bottom: 1rem;
-}
+    li {
+        border: 1px solid #ccc;
+        display: flex;
+        justify-content: space-between;
+        padding: .5rem 2rem;
+        margin-bottom: 1rem;
+    }
 
-.rm {
-  background: red;
-  color: #ffffff;
-  border-radius: 0%;
-  font-weight: bold;
-}
-input {
-  margin-right: 1rem;
-}
+    .rm {
+        background: red;
+        color: #ffffff;
+        border-radius: 0%;
+        font-weight: bold;
+    }
 
-.done {
-  text-decoration: line-through;
-}
+    input {
+        margin-right: 1rem;
+    }
 
-form {
-  display: flex;
-  padding: 1rem;
-}
+    .done {
+        text-decoration: line-through;
+    }
 
-.textcg {
-  width: 500px;
-  padding: 0.3rem;
-}
+    form {
+        display: flex;
+        padding: 1rem;
+    }
 
-.butcg {
-  width: 300px;
-}
+    .textcg {
+        width: 500px;
+        padding: 0.3rem
+    }
 
-.ra {
-  font-weight: bold;
-  width: 150px;
-}
+    .butcg {
+        width: 300px;
+    }
+
+    .ra {
+        font-weight: bold;
+        width: 150px;
+    }
 </style>
